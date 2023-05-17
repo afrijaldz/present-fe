2f8ab99850a8d2e06317021321070914f3c1ea28
Afrijal Dzuhri <ijuldz@gmail.com>
2023-05-17 17:09:52 +0700

    fix tailwind issue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import HeaderComponent from '@/components/HeaderComponent.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <HeaderComponent />
 
   <RouterView />
 </template>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const oke = ref('')
+</script>
+
+<template>
+  <header class="bg-blue-500 w-full h-full py-4">
+    <nav class="container m-auto">
+      <RouterLink class="text-green-100" to="/">Home</RouterLink>
+      <RouterLink to="/submit">Submit</RouterLink>
+    </nav>
+  </header>
+</template>
